c17eb5d8c9cce144f97b7b0b1eb7e7299aeed1ca
lucas <lucasherrera3009@gmail.com>
2023-08-01 02:31:45 +0200

    modifique: administrarj, carrito. falta terminar productos y el resto de los models

--- models/administrador/administrarPedidos_model.php
+++ models/administrador/administrarPedidos_model.php
@@ -1,93 +1,124 @@
 <?php
-	require_once('../../db/conectar.php');
+require_once('../../db/conectar.php');
 
-	class administrarPedidos_model{
-		private $db;
-		private $pedidos;
-		private $pedidosRealizados;
+class administrarPedidos_model
+{
+	private $db;
+	private $pedidos;
+	private $pedidosRealizados;
 
 
-		public function __construct(){
-			$this->db = Conectar::conexion();
-			$this->pedidos = array();
-			$this->pedidosRealizados = array();
+	public function __construct()
+	{
+		$this->db = Conectar::conexion();
+		$this->pedidos = array();
+		$this->pedidosRealizados = array();
+	}
+
+	public function getPedidos()
+	{
+		$query = "SELECT idPedido FROM Pedidos WHERE Estado = 'En proceso'";
+		$consulta = $this->db->query($query);
+
+		while ($filas = $consulta->fetch_assoc()) {
+			$this->pedidos[] = $filas;
 		}
 
-		public function getPedidos(){
-			$sql = "SELECT idPedido FROM Pedidos WHERE Estado = 'En proceso'";
-				$consulta = $this->db->query($sql);
-				
-				while($filas = $consulta->fetch_assoc()){
-					$this->pedidos[] = $filas;
-				}
-				
-				return $this->pedidos;
+		return $this->pedidos;
+	}
+
+	public function getPedidosRealizados()
+	{
+		$query = "SELECT idPedido FROM Pedidos WHERE Estado = 'Realizado'";
+		$consulta = $this->db->query($query);
+
+		while ($filas = $consulta->fetch_assoc()) {
+			$this->pedidosRealizados[] = $filas;
 		}
 
-		public function getPedidosRealizados(){
-			$sql = "SELECT idPedido FROM Pedidos WHERE Estado = 'Realizado'";
-				$consulta = $this->db->query($sql);
-				
-				while($filas = $consulta->fetch_assoc()){
-					$this->pedidosRealizados[] = $filas;
-				}
-				
-				return $this->pedidosRealizados;
-		}
+		return $this->pedidosRealizados;
+	}
 
-		public function confirmarPedido($idPedido){
-			$sql = "UPDATE Pedidos SET Estado = 'Realizado' WHERE Estado = 'En proceso' and idPedido = $idPedido";
-			$this->db->query($sql);
-		}
+	public function confirmarPedido($idPedido)
+	{
+		$query = "UPDATE Pedidos SET Estado = 'Realizado' WHERE Estado = 'En proceso' and idPedido = ?";
+		$consulta = $this->db->prepare($query);
 
-
-		function restarCantidadProducto($idProducto) {
-			$sql = "SELECT Stock FROM Productos WHERE idProducto = " . $idProducto;
-			$consulta = $this->db->query($sql);
-			$fila = $consulta->fetch_assoc();
-			$stock = $fila['Stock'];
-		
-			if ($stock == 1) {
-				$sql = "UPDATE Productos SET Stock = Stock - 1, Estado = 'Inactivo' WHERE idProducto = " . $idProducto;
-			} else {
-				$sql = "UPDATE Productos SET Stock = Stock - 1 WHERE idProducto = " . $idProducto;
-			}
-		
-			$this->db->query($sql);
-		}
-
-
-
-		public function pedidoLevantado($idPedido){
-			//Restar el producto comprado
-			$sql = "SELECT p.Nombre, p.Color, p.Talle, p.idProducto FROM Productos p
-        	INNER JOIN Contiene c ON c.idProducto = p.idProducto
-    		WHERE c.idPedido = " . $idPedido;
-			$consulta = $this->db->query($sql);
-			$productos = array(); 
-
-			while ($filas = $consulta->fetch_assoc()) {
-    			$productos[] = $filas;
-    			$this->restarCantidadProducto($filas['idProducto']);
-			}
-
-			//Cambiar el estado del pedido
-			$sql = "UPDATE Pedidos SET Estado = 'Retirado' WHERE Estado = 'Realizado' and idPedido =".$idPedido;
-			$this->db->query($sql);
-		}
-
-		public function getProductosDelPedido($idPedido){
-			$sql = "SELECT p.Nombre, p.Color, p.Talle FROM Productos p
-			INNER JOIN Contiene c ON c.idProducto = p.idProducto
-			WHERE c.idPedido = " . $idPedido;
-			$consulta = $this->db->query($sql);
-			$productos = array(); // Inicializar la variable $productos
-	
-			while($filas = $consulta->fetch_assoc()){
-				$productos[] = $filas;
-			}
-	
-			return $productos;
+		if ($consulta) {
+			$consulta->bind_param("i", $idPedido);
+			$consulta->execute();
 		}
 	}
-?>
+
+
+
+	function restarCantidadProducto($idProducto)
+	{
+		$query = "SELECT Stock FROM Productos WHERE idProducto = ?";
+		$consulta = $this->db->prepare($query);
+
+		if ($consulta) {
+			$consulta->bind_param("i", $idProducto);
+			$consulta->execute();
+			$result = $consulta->get_result();
+			$fila = $result->fetch_assoc();
+			$stock = $fila['Stock'];
+
+			if ($stock == 1) {
+				$query = "UPDATE Productos SET Stock = Stock - 1, Estado = 'Inactivo' WHERE idProducto = ?";
+			} else {
+				$query = "UPDATE Productos SET Stock = Stock - 1 WHERE idProducto = ?";
+			}
+
+			$consulta = $this->db->prepare($query);
+
+			if ($consulta) {
+				$consulta->bind_param("i", $idProducto);
+				$consulta->execute();
+				$consulta->close();
+			}
+		}
+	}
+
+
+
+
+	public function pedidoLevantado($idPedido)
+	{
+		//Restar el producto comprado
+		$query = "SELECT p.Nombre, p.Color, p.Talle, p.idProducto FROM Productos p
+        	INNER JOIN Contiene c ON c.idProducto = p.idProducto
+    		WHERE c.idPedido = " . $idPedido;
+		$consulta = $this->db->query($query);
+		$productos = array();
+
+		while ($filas = $consulta->fetch_assoc()) {
+			$productos[] = $filas;
+			$this->restarCantidadProducto($filas['idProducto']);
+		}
+
+		//Cambiar el estado del pedido
+		$query = "UPDATE Pedidos SET Estado = 'Retirado' WHERE Estado = 'Realizado' and idPedido = ?";
+		$consulta = $this->db->prepare($query);
+
+		if ($consulta) {
+			$consulta->bind_param("i", $idPedido);
+			$consulta->execute();
+		}
+	}
+
+	public function getProductosDelPedido($idPedido)
+	{
+		$query = "SELECT p.Nombre, p.Color, p.Talle FROM Productos p
+			INNER JOIN Contiene c ON c.idProducto = p.idProducto
+			WHERE c.idPedido = " . $idPedido;
+		$consulta = $this->db->query($query);
+		$productos = array(); // Inicializar la variable $productos
+
+		while ($filas = $consulta->fetch_assoc()) {
+			$productos[] = $filas;
+		}
+
+		return $productos;
+	}
+}
--- models/administrador/administrarRegistros_model.php
+++ models/administrador/administrarRegistros_model.php
@@ -1,54 +1,64 @@
 <?php
-	require_once('../../db/conectar.php');
+require_once('../../db/conectar.php');
 
-	class administrarRegistros_model{
-		
-		//Declaramos atributos privados
-		private $db;
-		private $usuarios;
-		
-		//Declaramos un constructor que sirve para incializar los atributos
-		public function __construct(){
-			
-			//Asignamos al atributo db el valor del metodo conexion() de la clase Conectar
-			//Conectar es la clase y conexion es el metodo
-			$this->db = Conectar::conexion();
-			//Determinamos que el atributo personas será un array
-			$this->usuarios = array();
-			
-		}
+class administrarRegistros_model
+{
 
-        //Obtener registros pendientes
-        public function getRegistros(){
-            $sql = "SELECT idUsuario, Nombre, Correo FROM Usuarios WHERE Aceptado = false";
-            $consulta = $this->db->query($sql);
+    //Declaramos atributos privados
+    private $db;
+    private $usuarios;
 
-            if ($consulta->num_rows > 0) {
-                while($filas = $consulta->fetch_assoc()){
-                    $this->usuarios[] = $filas;
-                }
-                return $this->usuarios;
-            } else {
-                return false;
+    //Declaramos un constructor que sirve para incializar los atributos
+    public function __construct()
+    {
+
+        //Asignamos al atributo db el valor del metodo conexion() de la clase Conectar
+        //Conectar es la clase y conexion es el metodo
+        $this->db = Conectar::conexion();
+        //Determinamos que el atributo personas será un array
+        $this->usuarios = array();
+    }
+
+    //Obtener registros pendientes
+    public function getRegistros()
+    {
+        $sql = "SELECT idUsuario, Nombre, Correo FROM Usuarios WHERE Aceptado = false";
+        $consulta = $this->db->query($sql);
+
+        if ($consulta->num_rows > 0) {
+            while ($filas = $consulta->fetch_assoc()) {
+                $this->usuarios[] = $filas;
             }
-        }
-
-        public function aceptarUsuario($idUsuario){       
-            // Actualizar el valor de 'Aceptado' a true
-            $sql = "UPDATE Usuarios SET Aceptado = true WHERE idUsuario = $idUsuario";
-            $query = "INSERT INTO Pedidos (Estado, idUsuario) VALUES ('Iniciado', $idUsuario)";
-            $this->db->query($query);
-            if( $this->db->query($sql)){
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        public function denegarUsuario($idUsuario){       
-            // Actualizar el valor de 'Aceptado' a true
-            $sql = "DELETE FROM Usuarios WHERE idUsuario = $idUsuario";
-            $denegar = $this->db->query($sql);
+            return $this->usuarios;
+        } else {
+            return false;
         }
     }
-?>
+
+    public function aceptarUsuario($idUsuario)
+    {
+        // Actualizar el valor de 'Aceptado' a true
+        $sql = "UPDATE Usuarios SET Aceptado = true WHERE idUsuario = ?";
+        $query = "INSERT INTO Pedidos (Estado, idUsuario) VALUES ('Iniciado', ?)";
+        $consulta = $this->db->prepare($sql, $query);
+        $consulta->bind_param("i", $idUsuario);
+        $consulta->execute();
+        $consulta->close();
+
+        if ($consulta) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function denegarUsuario($idUsuario)
+    {
+        // Actualizar el valor de 'Aceptado' a true
+        $query = "DELETE FROM Usuarios WHERE idUsuario = ?";
+        $consulta = $this->db->prepare($query);
+        $consulta->bind_param("i", $idUsuario);
+        $consulta->execute();
+        $consulta->close();
+    }
+}
