0b84f58e0a00f496a75e1675687ff951bb61f416
lucas <lucasherrera3009@gmail.com>
2023-08-06 02:17:13 +0200

    arregle algunas redirecciones

--- controllers/ingreso/registro_controller.php
+++ controllers/ingreso/registro_controller.php
@@ -1,42 +1,42 @@
 <?php
-	//Llamada al modelo
-	require_once("../../models/registro_model.php");
+//Llamada al modelo
+require_once("../../models/registro_model.php");
 
 
-    //Creamos un objeto de la clase UsuarioModel
-	$controlador = new RegistroUsuarioModel();
+//Creamos un objeto de la clase UsuarioModel
+$controlador = new RegistroUsuarioModel();
 
 
-    if (isset($_POST['iniciar'])){
-		$idProducto = $_POST['detallesProducto'];
-		header('Location: inicioSesion_controller.php?detallesProducto=' . urlencode($idProducto));
-		exit();
-	}
+if (isset($_POST['iniciar'])) {
+    $idProducto = $_POST['detallesProducto'];
+    header('Location: inicioSesion_controller.php?detallesProducto=' . urlencode($idProducto));
+    exit();
+}
 
-    if(isset($_POST['volverInicio'])){
+if (isset($_POST['volverInicio'])) {
 
-        header('location: /proyectoprogramacion');
-		exit();
+    header('location: /totallook/');
+    exit();
+}
+
+if (isset($_POST['Registrar'])) {
+    echo "Registrado";
+    $usuario = $_POST['usuario'];
+    $correo = $_POST['correo'];
+    $telefono = $_POST['telefono'];
+    $celular = $_POST['celular'];
+    $clave = $_POST['clave'];
+    $resultado = $controlador->RegistroUsuario($usuario, $correo, $telefono, $celular, $clave);
+    if ($resultado) {
+        $idUsuario = $controlador->getIdUsuario($correo);
+        session_start();
+        $_SESSION['usuario'] = $idUsuario;
+        header('location: /totallook/');
+        exit();
+    } else {
+        echo "<script>alert('Los datos ingresados son incorrectos.');window.location.href = window.location.href;</script>";
     }
-
-    if (isset($_POST['Registrar'])) {
-        echo "Registrado";
-        $usuario = $_POST['usuario'];
-        $correo = $_POST['correo'];
-        $telefono = $_POST['telefono'];
-        $celular = $_POST['celular'];
-        $clave = $_POST['clave'];
-        $resultado = $controlador->RegistroUsuario($usuario, $correo, $telefono, $celular, $clave);
-        if ($resultado) {
-            $idUsuario = $controlador->getIdUsuario($correo);
-            session_start();
-            $_SESSION['usuario'] = $idUsuario;
-            header('location: /totallook/');
-            exit();
-        } else {
-            echo "<script>alert('Los datos ingresados son incorrectos.');window.location.href = window.location.href;</script>";
-        }
-    }
+}
 
 
-    require_once("../../views/ingreso/register_view.php");
+require_once("../../views/ingreso/register_view.php");
